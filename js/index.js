38930cb607023a61b84fc55499ad822294654380
Audrey Claris <audrey claris>
2020-07-03 20:41:49 +0200

    03: filling pages

--- js/index.js
+++ js/index.js
@@ -2,4 +2,11 @@ $(window).on('hashchange', function (e) {
     var hash = window.location.hash;
     console.log("window.location.hash : ", hash)
 
+    // if (hash == '#about') {
+    //     $('#portrait').css("background-color", "blue");
+    // }
+    // else {
+    //     $('#portrait').css("background-color", "pink");
+
+    // }
 });
