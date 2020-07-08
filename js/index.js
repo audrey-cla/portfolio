2f563f2801b2c4b8fd73dd73ddaf6504ba6c752b
Audrey Claris <audrey claris>
2020-07-08 20:02:59 +0200

    06: cv page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['home', 'about', 'cv', 'projects', 'contact'],
+    slidesNavigation: true,
+    scrollBar: true,
+});
+
 $(window).on('hashchange', function (e) {
     var hash = window.location.hash;
     console.log("window.location.hash : ", hash)
@@ -10,7 +16,17 @@ $(window).on('hashchange', function (e) {
 
     // }
 });
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
 
+    // gsap.to("h1", {
+    //     duration: 4,
+    //     x: 750,
+    //     rotation: 360,
+    //     ease: "none",
+    // });
+
+});
 
 
 
