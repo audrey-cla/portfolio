27ee22a3faa48a4acdb78579950b9577e33246d9
Audrey Claris <audrey claris>
2020-07-09 16:32:48 +0200

    testing online

--- js/index.js
+++ js/index.js
@@ -18,16 +18,36 @@ $(window).on('hashchange', function (e) {
 });
 $(document).ready(function () {
     $('.tooltipped').tooltip();
+    var tlportrait = new TimelineMax({ repeat: -1, repeatDelay: 1 });
+    var tlhome = new TimelineMax({ repeat: -1 });
 
-    // gsap.to("h1", {
-    //     duration: 4,
-    //     x: 750,
-    //     rotation: 360,
-    //     ease: "none",
-    // });
+    TweenMax.set("#visage", { rotation: 5, transformOrigin: '50% 100%' })
+    TweenMax.set("#hairback", { rotation: 5, transformOrigin: '50% 100%' })
+    TweenMax.set("#plant1", { rotation: 5, transformOrigin: '50% 100%' })
+    TweenMax.set("#plant2", { rotation: -5, transformOrigin: '50% 100%' })
+    TweenMax.set("#plant5", { rotation: -5, transformOrigin: '50% 100%' })
+    TweenMax.set("#plant6", { rotation: 3, transformOrigin: '50% 100%' })
 
+    TweenMax.set("#planttop1", { rotation: 3, transformOrigin: '100% 50%' })
+
+
+    tlhome
+        .to("#plant1", 2, { rotation: -5 })
+        .to("#plant2", 2, { rotation: 5 }, "<")
+        .to("#plant5", 2, { rotation: 5 }, "<")
+        .to("#planttop1", 2, { rotation: -3 }, "<")
+        .to("#plant6", 2, { rotation: -3 }, "<")
+        .to("#plant1", 2, { rotation: 5 })
+        .to("#plant2", 2, { rotation: -5 }, "<")
+        .to("#plant5", 2, { rotation: -5 }, "<")
+        .to("#planttop1", 2, { rotation: 3 }, "<")
+        .to("#plant6", 2, { rotation: 3 }, "<")
+
+
+
+    tlportrait
+        .to("#visage", 2, { rotation: -5 })
+        .to("#hairback", 2, { rotation: -3 }, "<")
+        .to("#visage", 2, { rotation: 5 })
+        .to("#hairback", 2, { rotation: 5 }, "<");
 });
-
-
-
-
